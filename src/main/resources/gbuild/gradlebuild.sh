f2e0a89bd42346bdf554f61c2d59346cba6967d0
sunhz <sunhz@asiainfo.com>
2016-09-07 16:49:18 +0800

    gradle build jar

--- src/main/resources/gbuild/gradlebuild.sh
+++ src/main/resources/gbuild/gradlebuild.sh
@@ -8,8 +8,8 @@ echo "cope build.gradle file !!!"
 cp /aifs01/users/tstusr11/applications/TD1_TST-S1/service-portal-web/WEB-INF/classes/gbuild/build.gradle /aifs01/users/tstusr11/applications/service-sdk-fat
 
 echo "gradle build starting!"
-cd /aifs01/users/tstusr11/applications/service-sdk-fat && gradle publish
+cd /aifs01/users/tstusr11/applications/service-sdk-fat && gradle build jar
 
-scp /aifs01/users/tstusr11/applications/service-sdk-fat/iPaaS_DownLoad_Resource/com/ai/ipaas-sdk/0.2.1/ipaas-sdk-0.2.1.jar devusr02@10.1.228.198:/aifs01/devusers/devusr02/iPaaS_DownLoad_Resource/SDK
+#scp /aifs01/users/tstusr11/applications/service-sdk-fat/iPaaS_DownLoad_Resource/com/ai/ipaas-sdk/0.2.1/ipaas-sdk-0.2.1.jar devusr02@10.1.228.198:/aifs01/devusers/devusr02/iPaaS_DownLoad_Resource/SDK
 
 echo "Done!"
