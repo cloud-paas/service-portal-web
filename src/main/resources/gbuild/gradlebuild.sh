abf566e9f59ab5469789234a75961ca3565e71cf
sunhz <sunhz@asiainfo.com>
2016-09-12 17:24:20 +0800

    build文件修正

--- src/main/resources/gbuild/gradlebuild.sh
+++ src/main/resources/gbuild/gradlebuild.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-echo \"gradlebuild exec starting !!!\"
-export JAVA_HOME=/opt/freeware/jdk1.7.0_71
-export PATH=$JAVA_HOME/bin:$PATH
-export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
-
-echo \"cope build.gradle file !!!\"
-cp /aifs01/users/tstusr11/applications/TD1_TST-S1/service-portal-web/WEB-INF/classes/gbuild/build.gradle /aifs01/users/tstusr11/applications/service-sdk-fat
-
-echo \"gradle build starting!\"
-cd /aifs01/users/tstusr11/applications/service-sdk-fat && gradle build jar
-
-#scp /aifs01/users/tstusr11/applications/service-sdk-fat/iPaaS_DownLoad_Resource/com/ai/ipaas-sdk/0.2.1/ipaas-sdk-0.2.1.jar devusr02@10.1.228.198:/aifs01/devusers/devusr02/iPaaS_DownLoad_Resource/SDK
-
+SRC_HOME=/aifs01/devusr01/applications/service-sdk-fat
+echo "cope build.gradle file !!!"
+echo $PWD
+cp ./build.gradle ${SRC_HOME}
+echo "gradle build starting!"
+gradle build jar -p ${SRC_HOME}
+scp ${SRC_HOME}/build/libs/service-sdk-fat-1.0.jar devusr02@10.1.228.198:/aifs01/devusers/devusr02/iPaaS_DownLoad_Resource/SDK
 echo "Done!"
