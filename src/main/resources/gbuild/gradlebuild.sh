058ffb909538a352c36652e408faf021f0e36699
sunhz <sunhz@asiainfo.com>
2016-09-12 19:19:20 +0800

    update

--- src/main/resources/gbuild/gradlebuild.sh
+++ src/main/resources/gbuild/gradlebuild.sh
@@ -6,4 +6,3 @@ cp ./build.gradle ${SRC_HOME}
 echo "gradle build starting!"
 gradle build jar -p ${SRC_HOME}
 scp ${SRC_HOME}/build/libs/service-sdk-fat-1.0.jar devusr02@10.1.228.198:/aifs01/devusers/devusr02/iPaaS_DownLoad_Resource/SDK
-echo "Done!"
